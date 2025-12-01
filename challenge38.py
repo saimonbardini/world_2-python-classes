1db9a7f723405420767ffe526ae979e592a9a83d
Saimon Bardini <saimonmoncani@gmail.com>
2025-12-01 20:24:10 -0300

    Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem: O primeiro valor é maior. O segundo valor é maior. Não existe valor maior, os dois são iguais.

---
+++ challenge38.py
@@ -0,0 +1,9 @@
+n1 = int(input('insert the first number: '))
+n2 = int(input('insert the second number: '))
+
+if n1 > n2:
+    print('the first value is bigger. ')
+elif n2 > n1:
+    print('the second valur is bigger. ')
+elif n1 == n2:
+    print('they are both the same.')
